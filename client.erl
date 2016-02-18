06316f64f6810dce386d5d395a5cb63987ad0966
Mats Högberg <mats@hgbrg.se>
2016-02-18 09:28:32 +0100

    Added debug statements to client

--- client.erl
+++ client.erl
@@ -41,14 +41,18 @@ handle(State, {msg_from_GUI, Channel, Message}) ->
 
 %% Get current nick
 handle(State, whoami) ->
+    io:fwrite("~p: whoami~n", [State#client_state.nick]),
     {reply, State#client_state.nick, State};
 
 %% Change nick
 handle(State, {nick, Nick}) ->
+    io:fwrite("~p: Change nick to ~p~n", [State#client_state.nick, Nick]),
     {Data, NewState} = case State#client_state.server of
         undefined ->
+            io:fwrite("~p: Nick changed to ~p~n", [State#client_state.nick, Nick]),
             {ok, State#client_state{nick = Nick}};
         _ ->
+            io:fwrite("~p: User already connected~n", [State#client_state.nick]),
             {{error, user_already_connected, "Can't change nick when connected to a server"}, State}
     end,
     {reply, Data, NewState};
@@ -59,39 +63,39 @@ handle(State = #client_state { gui = GUIName }, {incoming_msg, Channel, Name, Me
     {reply, ok, State}.
 
 request(Server, Request, OldState, NewState) ->
-    io:fwrite("Client: Sending request ~p~n", [Request]),
+    io:fwrite("~p: Sending request ~p~n", [OldState#client_state.nick, Request]),
     % Check the state of the server to see if the client is connected
     {Data, State} = case Server of
         % If server is undefined the user is not connected and we respond with an error
         undefined ->
-            io:fwrite("Client: Server undefined~n"),
+            io:fwrite("~p: Server undefined~n", [OldState#client_state.nick]),
             {{error, user_not_connected, "Not connected to server"}, OldState};
         % Otherwise we send the request
         _ ->
             case catch genserver:request(Server, Request) of
                 % If the server respons with ok we send back the new state
                 ok ->
-                    io:fwrite("Client: Response ok~n"),
+                    io:fwrite("~p: Response ok~n", [OldState#client_state.nick]),
                     {ok, NewState};
                 % If the server responds with an error we leave the state unchanged and send back the error
                 {error, user_already_connected} ->
-                    io:fwrite("Client: User already connected~n"),
+                    io:fwrite("~p: User already connected~n", [OldState#client_state.nick]),
                     {{error, user_already_connected, "Already connected to server"}, OldState};
                 {error, user_not_connected} ->
-                    io:fwrite("Client: User not connected~n"),
+                    io:fwrite("~p: User not connected~n", [OldState#client_state.nick]),
                     {{error, user_not_connected, "Not connected to server"}, OldState};
                 {error, leave_channels_first} ->
-                    io:fwrite("Client: Leave channels first~n"),
+                    io:fwrite("~p: Leave channels first~n", [OldState#client_state.nick]),
                     {{error, leave_channels_first, "Leave channels before disconnecting"}, OldState};
                 {error, user_already_joined} ->
-                    io:fwrite("Client: User already joined~n"),
+                    io:fwrite("~p: User already joined~n", [OldState#client_state.nick]),
                     {{error, user_already_joined, "Channel already joined"}, OldState};
                 {error, user_not_joined} ->
-                    io:fwrite("Client: User not joined~n"),
+                    io:fwrite("~p: User not joined~n", [OldState#client_state.nick]),
                     {{error, user_not_joined, "Not in channel"}, OldState};
                 % If the request exits in any way the server could not be reached
                 {'EXIT', _Reason} ->
-                    io:fwrite("Client: EXIT reason ~p~n", [_Reason]),
+                    io:fwrite("~p: EXIT reason ~p~n", [OldState#client_state.nick, _Reason]),
                     {{error, server_not_reached, "Server not reached"}, OldState}
             end
     end,
