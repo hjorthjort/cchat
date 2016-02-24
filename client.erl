255d10fc55e474ca0cf6492937be176a15219958
Mats Högberg <mats@hgbrg.se>
2016-02-24 16:42:29 +0100

    Refactored request in client

--- client.erl
+++ client.erl
@@ -20,30 +20,43 @@ handle(State, {connect, Server}) ->
     case State#client_state.server of
         undefined ->
             ServerAtom = list_to_atom(Server),
-            request(ServerAtom, {connect, self(), State#client_state.nick}, State, State#client_state{server = ServerAtom});
+            case request(ServerAtom, {connect, self(), State#client_state.nick}) of
+                ok ->
+                    NewState = State#client_state{server = ServerAtom},
+                    {reply, ok, NewState};
+                Response ->
+                    {reply, Response, State}
+            end;
         _Server ->
             {reply, {error, user_already_connected, "Already connected to server"}, State}
     end;
 
 %% Disconnect from server
 handle(State, disconnect) ->
-    case State#client_state.server of
-        undefined ->
-            {reply, {error, user_not_connected, "Not connected to server"}, State};
-        Server ->
-            case State#client_state.channels of
-                [] ->
-                    request(Server, {disconnect, self()}, State, State#client_state{server = undefined});
-                [_H | _T ] ->
-                    {reply, {error, leave_channels_first, "Leave channels before disconnecting"}, State}
-            end
+    case State#client_state.channels of
+        [] ->
+            case request(State#client_state.server, {disconnect, self()}) of
+                ok ->
+                    NewState = State#client_state{server = undefined},
+                    {reply, ok, NewState};
+                Response ->
+                    {reply, Response, State}
+            end;
+        [_H | _T ] ->
+            {reply, {error, leave_channels_first, "Leave channels before disconnecting"}, State}
     end;
 
 % Join channel
 handle(State, {join, Channel}) ->
     case lists:member(Channel, State#client_state.channels) of
         false ->
-            request(State#client_state.server, {join, Channel, self()}, State, State#client_state{ channels = [ Channel | State#client_state.channels ]});
+            case request(State#client_state.server, {join, Channel, self()}) of
+                ok ->
+                    NewState = State#client_state{ channels = [ Channel | State#client_state.channels ]},
+                    {reply, ok, NewState};
+                Response ->
+                    {reply, Response, State}
+            end;
         true ->
             {reply, {error, user_already_joined, "Channel already joined"}, State}
     end;
@@ -52,23 +65,29 @@ handle(State, {join, Channel}) ->
 handle(State, {leave, Channel}) ->
     case lists:member(Channel, State#client_state.channels) of
         true ->
-            request(State#client_state.server, {leave, Channel, self()}, State, State#client_state{ channels = lists:delete(Channel, State#client_state.channels) });
+            case request(State#client_state.server, {leave, Channel, self()}) of
+                ok ->
+                    NewState = State#client_state{ channels = lists:delete(Channel, State#client_state.channels) },
+                    {reply, ok, NewState};
+                Response ->
+                    {reply, Response, State}
+            end;
         false ->
             {reply, {error, user_not_joined, "Not in channel"}, State}
     end;
 
 % Sending messages
 handle(State, {msg_from_GUI, Channel, Message}) ->
-    case State#client_state.server of
-        undefined ->
-            {reply, {error, user_not_connected, "Not connected to server"}, State};
-        Server ->
-            case lists:member(Channel, State#client_state.channels) of
-                true ->
-                    request(Server, {send_message, Channel, Message, self()}, State, State);
-                false ->
-                    {reply, {error, user_not_joined, "Not in channel"}, State}
-            end
+    case lists:member(Channel, State#client_state.channels) of
+        true ->
+            case request(State#client_state.server, {send_message, Channel, Message, self()}) of
+                ok ->
+                    {reply, ok, State};
+                Response ->
+                    {reply, Response, State}
+            end;
+        false ->
+            {reply, {error, user_not_joined, "Not in channel"}, State}
     end;
 
 %% Get current nick
@@ -99,41 +118,24 @@ handle(State = #client_state { gui = GUIName }, {incoming_msg, Channel, Name, Me
 %% request/4 sends a request to a specified PID (Server). If the response from
 %% the server is `ok` this function returns {reply, ok, NewState}. If the
 %% response is an error it returns {reply, {error, atom, Message}, OldState}.
-request(Server, Request, OldState, NewState) ->
-    io:fwrite("~p: Sending request ~p~n", [OldState#client_state.nick, Request]),
+request(Server, Request) ->
+    io:fwrite("Sending request ~p~n", [Request]),
     % Check the state of the server to see if the client is connected
-    {Data, State} = case Server of
+    case Server of
         % If server is undefined the user is not connected and we respond with an error
         undefined ->
-            io:fwrite("~p: Server undefined~n", [OldState#client_state.nick]),
-            {{error, user_not_connected, "Not connected to server"}, OldState};
+            io:fwrite("Server undefined~n", []),
+            {error, user_not_connected, "Not connected to server"};
         % Otherwise we send the request
         _ ->
             case catch genserver:request(Server, Request) of
                 % If the server responds with `ok` we return the new state
                 ok ->
-                    io:fwrite("~p: Response ok~n", [OldState#client_state.nick]),
-                    {ok, NewState};
-                % If the server responds with an error we leave the state unchanged and return the error
-                {error, user_already_connected} ->
-                    io:fwrite("~p: User already connected~n", [OldState#client_state.nick]),
-                    {{error, user_already_connected, "Already connected to server"}, OldState};
-                {error, user_not_connected} ->
-                    io:fwrite("~p: User not connected~n", [OldState#client_state.nick]),
-                    {{error, user_not_connected, "Not connected to server"}, OldState};
-                {error, leave_channels_first} ->
-                    io:fwrite("~p: Leave channels first~n", [OldState#client_state.nick]),
-                    {{error, leave_channels_first, "Leave channels before disconnecting"}, OldState};
-                {error, user_already_joined} ->
-                    io:fwrite("~p: User already joined~n", [OldState#client_state.nick]),
-                    {{error, user_already_joined, "Channel already joined"}, OldState};
-                {error, user_not_joined} ->
-                    io:fwrite("~p: User not joined~n", [OldState#client_state.nick]),
-                    {{error, user_not_joined, "Not in channel"}, OldState};
+                    io:fwrite("Response ok~n", []),
+                    ok;
                 % If the request exits in any way the server could not be reached
                 {'EXIT', _Reason} ->
-                    io:fwrite("~p: EXIT reason ~p~n", [OldState#client_state.nick, _Reason]),
-                    {{error, server_not_reached, "Server not reached"}, OldState}
+                    io:fwrite("EXIT reason ~p~n", [_Reason]),
+                    {error, server_not_reached, "Server not reached"}
             end
-    end,
-    {reply, Data, State}.
+    end.
